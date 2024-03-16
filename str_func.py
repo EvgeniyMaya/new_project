b9c68240248e8a75546d9841d18cdfd43305573a
EvgeniyMaya <super89048368898@yandex.ry>
2024-03-16 18:18:52 +0400

    fun string_up + docstring fun 2 + docstring

--- str_func.py
+++ str_func.py
@@ -1,6 +1,10 @@
 def fun(string):
+    """функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     string_up = str(string).upper()
     return string_up
 
-
+def fun2(string):
+    """которая делает заглавными первые буквы каждого слова в строке"""
+    string_1=str(string).split(' ')
+    return " ".join(string_1).title()
 
